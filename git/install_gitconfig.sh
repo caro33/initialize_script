6810f4f7144e0963335d3ba338d93225dd946692
caro33 <caro33@naver.com>
2021-05-25 22:37:11 +0900

    Update install_gitconfig.sh

--- git/install_gitconfig.sh
+++ git/install_gitconfig.sh
@@ -3,4 +3,4 @@ BASE_DIR=$(dirname "$0")
 CUR_DIR=$(pwd)
 
 
-curl https://raw.githubusercontent.com/ohilho/initialize_script/master/git/.gitconfig > ${HOME}/.gitconfig
+curl https://raw.githubusercontent.com/caro33/initialize_script/master/git/.gitconfig > ${HOME}/.gitconfig
